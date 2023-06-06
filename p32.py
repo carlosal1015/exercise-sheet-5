cfa8de38209c7bc257566984f4d20791dbe2cd9a
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-06-06 14:45:03 -0500

    Update

--- p32.py
+++ p32.py
@@ -4,18 +4,21 @@
 
 import sympy as sp
 
-R = sp.Matrix([[1, 1], [0, 1]])
-D = sp.eye(2)
-R_1 = D * R
+A = sp.Matrix([[1, 0, 0], [2, 1, -1]])
+Q, R = A.QRdecomposition()
+G = (A.T * A)#.cholesky()
+sigma = G.eigenvals()
 
 if __name__ == "__main__":
+  print("A=")
+  sp.pprint(A)
+  print(f"rank(A)={A.rank()}")
+  print("Q=")
+  sp.pprint(Q)
   print("R=")
   sp.pprint(R)
-  print(f"¿R es triangular superior? {R.is_upper}.")
-  print(f"|R|={R.det()}.")
-  print("D=")
-  sp.pprint(D)
-  print("R_1=")
-  sp.pprint(R_1)
-  print(f"¿R_1 es triangular superior? {R_1.is_upper}.")
-  print(f"|R_1|={R_1.det()}.")
+  assert (Q * Q.T).equals(sp.Identity(2))
+  print("G=")
+  sp.pprint(G)
+  sp.pprint()
+  # assert (A.T * A).equals(G * G.T)
