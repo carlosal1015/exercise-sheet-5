a0989c583e0ca8c365b8fe5e05e44081959b3d9f
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-06-10 00:09:35 -0500

    Formatting

--- p10.py
+++ p10.py
@@ -5,20 +5,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams["text.usetex"] = True
+plt.rcParams["font.serif"] = ["Computer Modern"]
+
 nx = 101
 x = np.linspace(start=0.1, stop=2.0, num=nx)
-y = x * np.log(x)
+y = x * np.log(x) - 1
 
 
 @np.vectorize
 def constant_function(x):
-    return 1
+    return 0
 
 
 if __name__ == "__main__":
     fig, ax = plt.subplots()
-    ax.plot(x, y, label="Curve")
-    ax.plot(x, constant_function(x), label="Line")
-    ax.legend()
+    ax.plot(x, y, label=r"$x\ln(x)-1$", lw=0.5)
+    ax.plot(x, constant_function(x), label=r"$0$", lw=0.5)
+    ax.legend(shadow=True, title="Leyenda", fancybox=True)
     # plt.show()
-    fig.savefig("p10.pdf")
+    fig.savefig("p10.pdf", transparent=True, bbox_inches="tight")
